6f85c3a249f2c4350764117cd1ea44710a1a15a5
YanQinghao <woshiyanqinghao@gmail.com>
2020-12-16 17:38:02 +0800

    data utils

---
+++ utils/data.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+
+import time
+
+
+def timestamp(self):
+    return int(time.time() * 1000)
+
+
+def collector(redis_client, time_interval, nodes):
+    time_now = timestamp()
+    time_left = time_now - time_interval * 1000
+    master_messages = redis_client.xrange("mq-master", str(time_left), str(time_now))
+    node_messages = {}
+    for nodeid in nodes:
+        node_messages[nodeid] = redis_client.xrange(f"mq-{nodeid}", str(time_left), str(time_now))
+    return master_messages, node_messages

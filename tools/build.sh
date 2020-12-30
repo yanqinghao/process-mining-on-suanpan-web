b19f214edf33ae363a356dc901a0f611c93c7ae3
YanQinghao <woshiyanqinghao@gmail.com>
2020-12-30 14:34:40 +0800

    fix build tools

--- tools/build.sh
+++ tools/build.sh
@@ -18,12 +18,11 @@ else
     BUILD_TAGS=("preview")
 fi
 BUILDNAMES=("suanpan-web-process-mining")
-REQUIREMENTS=("requirements.txt")
 NAMESPACE="shuzhi-amd64"
 for ((i = 0; i < ${#TAGS[@]}; i++)); do
     for ((j = 0; j < ${#BUILDNAMES[@]}; j++)); do
         docker build --build-arg NAME_SPACE=${NAMESPACE} --build-arg DOCKER_BASE=${DOCKERBASE} \
-            --build-arg PYTHON_VERSION=${TAGS[i]} --build-arg REQUIREMENTS_FILE=${REQUIREMENTS[j]} -t \
+            --build-arg PYTHON_VERSION=${TAGS[i]} -t \
             registry-vpc.cn-shanghai.aliyuncs.com/${NAMESPACE}/${BUILDNAMES[j]}:${BUILD_VERSIONS[i]} \
             -f docker/Dockerfile .
         docker push registry-vpc.cn-shanghai.aliyuncs.com/${NAMESPACE}/${BUILDNAMES[j]}:${BUILD_VERSIONS[i]}
